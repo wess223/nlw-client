a757b782d92d450647016204604afb4cd329cac0
Wesley Alves <wesley.info07@gmail.com>
2022-11-02 13:54:35 -0300

    requisição api next em ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,31 @@
+// interface HomeProps{
+//   count : number
+// }
 
-export default function Home() {
+export default function Home(props) {
+
   return (
-    <h1>next.js</h1>
+    <>
+      {
+        props.list.map(x => {
+          return (
+            <div>
+              {x.name}
+            </div>
+          )
+        })
+      }
+    </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('https://api.github.com/users/wess223/repos')
+  const repoList = await response.json()
+
+  return {
+    props: {
+      list: repoList
+    }
+  }
 }
